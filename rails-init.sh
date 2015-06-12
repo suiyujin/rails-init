18ccc1d13bc1fd63cda255ec88229fd4b8c20f0f
suiyujin <suiyujin04@gmail.com>
2015-06-12 16:38:28 +0900

    add message

--- rails-init.sh
+++ rails-init.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-read -p "rails app name:" rails_app_name
+read -p "rails app name: " rails_app_name
+read -p "database user: " database_user
+read -p "database password: " database_password
 
 rbenv global 2.2.2
 rbenv rehash
@@ -10,34 +12,46 @@ source 'http://rubygems.org'
 ruby '2.2.2'
 gem 'rails', '4.2.1'
 EOS
+echo "### Gemfile created. ###"
 
 bundle install --path vendor/bundle
 bundle exec rails new $rails_app_name -BJT -d mysql
 
 rm -rf Gemfile Gemfile.lock vendor/
+echo "### remove extra files ###"
 
 cd $rails_app_name
 
+echo "Gemfile:"
 curl "https://raw.githubusercontent.com/suiyujin/rails-init/master/Gemfile" -o Gemfile
+echo ".pryrc:"
 curl "https://raw.githubusercontent.com/suiyujin/rails-init/master/.pryrc" -o .pryrc
+echo ".gitignore:"
 curl "https://raw.githubusercontent.com/suiyujin/rails-init/master/.gitignore" -o .gitignore
+echo "development.rb:"
 curl "https://raw.githubusercontent.com/suiyujin/rails-init/master/development.rb" -o config/environments/development.rb
+echo "ja.yml:"
 curl "https://raw.githubusercontent.com/suiyujin/rails-init/master/ja.yml" -o config/locales/ja.yml
 
 bundle install --path vendor/bundle
 
 cp config/database.yml config/database.yml.sample
-sed -i "" -e "s/database: ${rails_app_name}_development/database: ${rails_app_name}_dev/" config/database.yml
-sed -i "" -e "s/database: ${rails_app_name}_production/database: ${rails_app_name}_pro/" config/database.yml
-echo "modify database.yml"
+
+sed -i "" -e "s/database: ${rails_app_name}_development$/database: ${rails_app_name}_dev/" config/database.yml
+sed -i "" -e "s/database: ${rails_app_name}_production$/database: ${rails_app_name}_pro/" config/database.yml
+sed -i "" -e "s/username: root$/username: $database_user/" config/database.yml
+sed -i "" -e "s/password:$/password: $database_password/" config/database.yml
+echo "### modify database.yml ###"
 
 bundle exec rake db:create
+echo "### database created. ###"
 bundle exec rake db:migrate
+echo "### database migrated. ###"
 
 sed -i "" -e "s/# config.time_zone = 'Central Time (US & Canada)'/config.time_zone = 'Tokyo'/" config/application.rb
-echo "reset time_zone"
+echo "### reset time_zone ###"
 sed -i "" -e "s/# config.i18n.default_locale = :de/config.i18n.default_locale = :ja/" config/application.rb
-echo "reset default_locale"
+echo "### reset default_locale ###"
 
 git init
 git add .
